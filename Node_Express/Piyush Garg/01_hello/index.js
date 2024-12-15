833bb2caaa79d236c5e7c30fbc0115634f1281d1
AnuragDahiwade <anurag.dahiwade@gmail.com>
2024-12-15 22:01:28 +0530

    Modules basics in nodejs

---
+++ Node_Express/Piyush Garg/01_hello/index.js
@@ -0,0 +1,11 @@
+console.log("Hello World!!");
+
+const math = require("./math");
+const { add, sub } = require("./math");
+
+// const fs = require("fs");
+// console.log(fs);
+
+console.log(math);
+console.log(add(3, 5));
+console.log(sub(3, 5));
